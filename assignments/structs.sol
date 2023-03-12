32233ea3c4769e0ce5afc6dcaa8a7f6a40ab3f91
Samuel Danso <me.samueldanso@gmail.com>
2023-03-12 22:16:52 +0000

    added: task 2.3.3

---
+++ assignments/structs.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract UserInfo {
+    
+    struct User {
+        string name;
+        uint256 age;
+    }
+    
+    mapping(address => User) public users;
+    
+    function setUserDetails(string calldata name, uint256 age) public {
+        User storage user = users[msg.sender];
+        user.name = name;
+        user.age = age;
+    }
+    
+    function getUserDetails() public view returns (string memory, uint256) {
+        User storage user = users[msg.sender];
+        return (user.name, user.age);
+    }
+    
+}
