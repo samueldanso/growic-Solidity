921e74ffb770052d7c6f50f0f1f5d55395359f4d
samueldanso <me.samueldanso@gmail.com>
2023-03-24 14:37:38 +0000

    added: task 2.3.7

---
+++ assignments/inheritance.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ArrisToken is ERC20 {
+    constructor() ERC20("ArrisToken", "ARIS") {}
+}
